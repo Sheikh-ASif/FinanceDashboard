0de131b6af30f1fcb35ce32425ab79f1e539dcc2
Sheikh-ASif <mdasif74083@gmail.com>
2026-04-02 13:48:12 +0530

    chnges

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',        // ensures correct paths for assets
+  build: {
+    outDir: 'dist',  // Vite default, good for static deployment
+  },
 })
